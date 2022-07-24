7e69a287db102bdc8d626653d629d53d53ae785e
jmwalsh91 <jordanmwalsh@gmail.com>
2022-07-24 15:21:43 -0600

    style: fix layout

--- blogcms-react/src/components/Shell.tsx
+++ blogcms-react/src/components/Shell.tsx
@@ -10,8 +10,8 @@ export default function Shell({children}: Props ) {
 
   return (
     <AppShell
-      navbar={<Navbar width={{ base: 300 }} height={500} p="xs"> <NavItems/> </Navbar>}
-      header={<Header height={60} p="xs">BlogCMS</Header>}
+      navbar={<Navbar width={{ base: 300 }} height="90vh" p="xs"> <NavItems/> </Navbar>}
+      header={<Header height="10vh" p="xs">BlogCMS</Header>}
       styles={(theme) => ({
         main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
       })}
