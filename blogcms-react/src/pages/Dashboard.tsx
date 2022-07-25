1d7de744668d544035bc774ccbd3ffba5a6dbb1c
jmwalsh91 <jordanmwalsh@gmail.com>
2022-07-25 03:33:12 -0600

    task: placeholder dash card

--- blogcms-react/src/pages/Dashboard.tsx
+++ blogcms-react/src/pages/Dashboard.tsx
@@ -1,32 +1,73 @@
-import { Grid, Paper } from '@mantine/core'
-import React from 'react'
-import { DashStats } from '../components/DashStats'
+import {
+  Button,
+  Card,
+  CardSection,
+  Grid,
+  Header,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core";
+import React from "react";
+import { DashStats } from "../components/DashStats";
 
-type Props = {}
+type Props = {};
 const statsData = [
-    {
-        title: 'Unique Views',
-        stats: '1000',
-        description: 'This is how many unique views, congrats!'
-},
-{
-    title: 'Posts this week',
-    stats: '7',
-    description: 'Up 700% from last week!'
-}
-,
-{
-title:'Another Metric',
-stats: 'Number',
-description: 'Another Metric Description would go here.'
-}]
+  {
+    title: "Unique Views",
+    stats: "1000",
+    description: "This is how many unique views, congrats!",
+  },
+  {
+    title: "Posts this week",
+    stats: "7",
+    description: "Up 700% from last week!",
+  },
+  {
+    title: "Another Metric",
+    stats: "Number",
+    description: "Another Metric Description would go here.",
+  },
+];
 function Dashboard({}: Props) {
   return (
-    <Paper>
-        <DashStats data={statsData}/>
+    <Paper
+      style={{
+        margin: "2rem",
+        width: "80%",
+        display: "flex",
+        justifySelf: "center",
+        justifyContent: "start",
+        flexDirection: "column",
+      }}
+    >
+      <DashStats data={statsData} />
+
+      <Paper p={".5rem"}>
+        <Text
+          size="xl"
+          component="h1"
+          align={"center"}
+          style={{
+            fontSize: "2rem",
+          }}
+        >
+          {" "}
+          Recent Posts
+        </Text>
+      </Paper>
+      <Card
+        style={{
+          width: "25rem",
+          height: "20rem",
+        }}
+      >
+        <Title order={3} p="1rem">This is a recent Post</Title>
+        <CardSection>This is a recent post</CardSection>
+        <Button>Expand</Button>
+      </Card>
     </Paper>
-    
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
