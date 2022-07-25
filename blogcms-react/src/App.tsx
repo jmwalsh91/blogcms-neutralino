4a705ab5364a0bd66d52ca00dc7ab98f87db5c26
jmwalsh91 <jordanmwalsh@gmail.com>
2022-07-25 00:21:23 -0600

    fix: unterminated comment in index.css

--- blogcms-react/src/App.tsx
+++ blogcms-react/src/App.tsx
@@ -1,30 +1,25 @@
-import { useState } from 'react'
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Shell from './components/Shell'
-import { Button, Header, MantineProvider, Paper } from '@mantine/core'
-import { theme } from './styles/theme'
-import Home from './pages/Home'
+import { useState } from "react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Shell from "./components/Shell";
+import { Button, Header, MantineProvider, Paper } from "@mantine/core";
+import { theme } from "./styles/theme";
+import Home from "./pages/Home";
 
 function App() {
-
-
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme
-    }>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
       <BrowserRouter>
-   
-  
-   <Shell>
-   <Routes>
-        <Route path="/" element={<Home/>}></Route>
-      </Routes>
- <Outlet/>
-       </Shell>
-       </BrowserRouter>
+        <Shell>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+          <Outlet />
+        </Shell>
+      </BrowserRouter>
     </MantineProvider>
-  )
+  );
 }
 
-export default App
+export default App;
