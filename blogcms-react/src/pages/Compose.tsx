66a1f27f76e47a911b60ba3f38fbea41e27b45be
jmwalsh91 <jordanmwalsh@gmail.com>
2022-07-29 23:58:24 -0600

    task: blogpost tags multiselect

--- blogcms-react/src/pages/Compose.tsx
+++ blogcms-react/src/pages/Compose.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, TextInput } from "@mantine/core";
+import { Button, Container, MultiSelect, TextInput } from "@mantine/core";
 import RichTextEditor from "@mantine/rte";
 import React, { useState } from "react";
 import { Routes } from "react-router-dom";
@@ -7,6 +7,33 @@ import { sb } from "../services/sb";
 const initialValue =
   "<p>Your initial <b>html value</b> or an empty string to init editor without value</p>";
 
+const tags = [
+  {value: 'tutorial' ,
+    label: 'tutorial'},
+    {value: 'React' ,
+      label: 'React'},
+      {value: 'Typescript' ,
+        label: 'Typescript'},
+        {value: 'Storybook',
+          label: 'Storybook'},
+          {value: 'Remix',
+            label: 'Remix'},
+            {value: 'Vercel',
+              label: 'Vercel'},
+              {value: 'React Query' ,
+                label: 'React Query'},
+                {value: 'SWR' ,
+                  label: 'SWR'},
+                  {value: 'MUI',
+                    label: 'MUI'},
+                    {value: 'Chrome MV3',
+                      label: 'Chrome MV3'},
+                    
+                  
+                
+              
+            
+        ]
 function Compose() {
   const [value, onChange] = useState<string>(initialValue);
   const [title, setTitle] = useState<string>("Title");
@@ -29,6 +56,11 @@ function Compose() {
         minHeight: "50vh",
         maxHeight: "85vh"
       }} onChange={onChange} />
+          <MultiSelect
+      data={tags}
+      label="Tags"
+      placeholder="Add tags to blogpost"
+    />
       <Button onClick={() => handleSubmitPost()}></Button>
     </Container>
   );
